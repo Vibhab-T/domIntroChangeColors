c30b4abb7f35fbe75e02444608408cc3eed74032
Vibhab-T <vibhabtimsina86@gmail.com>
2024-05-23 08:38:41 +0545

    removed the x,y,z variables and used direct function call for random numbers

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let box = document.querySelector(".box");
 
 box.addEventListener("click", () => {
-  let x = Math.floor(Math.random() * 255);
-  let y = Math.floor(Math.random() * 255);
-  let z = Math.floor(Math.random() * 255);
-  box.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-  box.style.color = `rgb(${y}, ${x}, ${z})`;
+  box.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+  box.style.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)})`;
 });
